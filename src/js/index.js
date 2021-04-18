27655664a19451f258d0713986425cc9201322e8
Przemyslaw Duniec <pduniec@outlook.com>
2021-04-18 20:56:29 +0200

    Add to project code js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,56 @@
 import '../scss/main.scss';
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+fetch('https://api.github.com/users/itzygzak/repos?sort=created&direction=asc')
+.then((res) => res.json())
+.then((res) => {
+    const container = document.querySelector('.project-grid--js');
+    for (let repo of res) {
+        const {description, homepage, html_url, name} = repo;
+
+        const template = `<article class="project">
+        <div class="project__window">
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+        </div>
+        <div class="project__content">
+          <img src="../assets/img/github.svg" alt="" />
+          <h3 class="project__grid project__title">
+            <span class="project__label">project:</span>
+            <span>${name}</span>
+          </h3>
+          <p class="project__grid project__grid--description">
+            <span class="project__label">description:</span>
+            <span>${description}</span>
+          </p>
+          <p class="project__grid">
+            <span class="project__label">demo:</span>
+            <span
+              >&lt;<a
+                class="project__link"
+                href="${homepage}"
+                title="${name}"
+                >see_here</a
+              >&lt;
+            </span>
+          </p>
+          <p class="project__grid">
+            <span class="project__label">github:</span>
+            <span
+              >&lt;<a
+                class="project__link"
+                href="${html_url}"
+                title="${name}"
+                >source_code</a
+              >&lt;</span
+            >
+          </p>
+        </div>
+      </article>`
+        container.innerHTML += template; 
+    }
+    
+})
+.catch((e) => console.log(e));
